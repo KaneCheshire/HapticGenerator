82101052b17d1b8b49682d7666cf1d09154e7a07
KaneCheshire <kane.cheshire@googlemail.com>
2017-04-26 21:32:57 +0100

    Fixes borked compilation

---
+++ Example/Pods/Target Support Files/Pods-HapticGenerator_Example-HapticGenerator_ExampleTests/Pods-HapticGenerator_Example-HapticGenerator_ExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HapticGenerator_Example_HapticGenerator_ExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_HapticGenerator_Example_HapticGenerator_ExampleTests
+@end
--- Example/Pods/Target Support Files/Pods-HapticGenerator_Example-HapticGenerator_Tests/Pods-HapticGenerator_Example-HapticGenerator_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HapticGenerator_Example_HapticGenerator_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_HapticGenerator_Example_HapticGenerator_Tests
-@end
