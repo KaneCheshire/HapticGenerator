c5e7c265f31276b5dc0e53dccb20658b2e91eed7
KaneCheshire <kane.cheshire@googlemail.com>
2017-04-27 08:57:58 +0100

    Updated example with an example

--- Example/HapticGenerator/ViewController.swift
+++ Example/HapticGenerator/ViewController.swift
@@ -7,18 +7,52 @@
 //
 
 import UIKit
+import HapticGenerator
 
 class ViewController: UIViewController {
+  
+  let selectionHapticGenerator = HapticGenerator(hapticType: .selection)
+  let lightImpactHapticGenerator = HapticGenerator(hapticType: .impact(.light))
+  let mediumImpactHapticGenerator = HapticGenerator(hapticType: .impact(.medium))
+  let heavyImpactHapticGenerator = HapticGenerator(hapticType: .impact(.heavy))
+  let successNotificationHapticGenerator = HapticGenerator(hapticType: .notification(.success))
+  let warningNotificationHapticGenerator = HapticGenerator(hapticType: .notification(.warning))
+  let errorNotificationHapticGenerator = HapticGenerator(hapticType: .notification(.error))
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  @IBAction private func selectionTapped(_ sender: UIButton) {
+    // Note that you can run this on pre-iOS 10 without an availability check, it just fails silently.
+    selectionHapticGenerator.generateHaptic()
+    selectionHapticGenerator.prepareForUse() // Keeps Taptic engine "spun up"
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  @IBAction private func lightImpactTapped(_ sender: UIButton) {
+    lightImpactHapticGenerator.generateHaptic()
+    lightImpactHapticGenerator.prepareForUse()
+  }
+  
+  @IBAction private func mediumImpactTapped(_ sender: UIButton) {
+    mediumImpactHapticGenerator.generateHaptic()
+    mediumImpactHapticGenerator.prepareForUse()
+  }
+  
+  @IBAction private func heavyImpactTapped(_ sender: UIButton) {
+    heavyImpactHapticGenerator.generateHaptic()
+    heavyImpactHapticGenerator.prepareForUse()
+  }
+  
+  @IBAction private func successNotificationTapped(_ sender: UIButton) {
+    successNotificationHapticGenerator.generateHaptic()
+    successNotificationHapticGenerator.prepareForUse()
+  }
+  
+  @IBAction private func warningNotificationTapped(_ sender: UIButton) {
+    warningNotificationHapticGenerator.generateHaptic()
+    warningNotificationHapticGenerator.prepareForUse()
+  }
+  
+  @IBAction private func errorNotificationTapped(_ sender: UIButton) {
+    errorNotificationHapticGenerator.generateHaptic()
+    errorNotificationHapticGenerator.prepareForUse()
+  }
 }
 
