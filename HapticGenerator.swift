8c42908caf6099061c090a25617a974209fd2b65
Suyash Srijan <suyashsrijan@outlook.com>
2018-08-24 22:42:15 +0100

    iPhone 6S support
    This commit adds support for haptic feedback on iPhone 6S and 6S Plus. The feedback is an approximation of the feedback generated by the Taptic Engine on iPhone 7 or above. On older devices, no feedback will be generated.

--- HapticGenerator.swift
+++ HapticGenerator.swift
@@ -3,6 +3,7 @@
 //  Created by Kane Cheshire on 16/12/2016.
 //
 import UIKit
+import AudioToolbox
 
 /// Generates haptics on supported devices.
 /// On devices that don't support haptics, the generator silently fails,
@@ -66,6 +67,20 @@ public struct Haptic {
         case notification(NotificationType)
     }
     
+    /// A struct that represents the types of haptic feedback (iPhone 6S/6S Plus only)
+    ///
+    /// - selectionOrSuccess: A feedback on selection or success notification
+    /// - impact: A feedback on impact
+    /// - error: A feedback on error
+    /// - warn: A feedback on warning
+    private struct HapticFeedbackIdentifier {
+        static let selection = SystemSoundID(1519)
+        static let impact = SystemSoundID(1520)
+        static let error = SystemSoundID(1521)
+        static let warn = SystemSoundID(1102)
+        static let success = SystemSoundID(4095)
+    }
+    
     let type: HapticType
     internal(set) var generator: Any? = nil
     
@@ -97,7 +112,7 @@ public struct Haptic {
     ///
     /// - Parameter prepareForReuse: If set to `true`, HapticGenerator will attempt to keep the taptic engine powered up for a few seconds, making it more responsive. Defaults to `false`.
     public func generate(prepareForReuse: Bool = false) {
-        if #available(iOS 10.0, *) {
+        if #available(iOS 10.0, *), hasTapticEngine() {
             switch type {
             case .selection: (generator as? UISelectionFeedbackGenerator)?.selectionChanged()
             case .impact: (generator as? UIImpactFeedbackGenerator)?.impactOccurred()
@@ -105,6 +120,17 @@ public struct Haptic {
             }
             if prepareForReuse {
                 prepareForUse()
+            }
+        } else if hasHapticEngine() {
+            switch type {
+            case .selection: AudioServicesPlaySystemSound(HapticFeedbackIdentifier.selection)
+            case .impact: AudioServicesPlaySystemSound(HapticFeedbackIdentifier.impact)
+            case .notification(let type):
+                switch(type) {
+                case .error: AudioServicesPlaySystemSound(HapticFeedbackIdentifier.error)
+                case .success: AudioServicesPlaySystemSound(HapticFeedbackIdentifier.success)
+                case .warning: AudioServicesPlaySystemSound(HapticFeedbackIdentifier.warn)
+                }
             }
         }
     }
@@ -121,5 +147,86 @@ public struct Haptic {
             (generator as? UIFeedbackGenerator)?.prepare()
         }
     }
+}
+
+// MARK: - Extension that contains methods to determine current device & supported device(s)
+extension Haptic {
     
+    // MARK: - Enums -
+    // MARK: Internal
+    
+    /// An enum that represents the type of device (iPhone)
+    ///
+    /// - iPhone6S: An iPhone 6S
+    /// - iPhone6SPlus: An iPhone 6S Plus
+    /// - iPhone7: An iPhone 7
+    /// - iPhone7Plus: An iPhone 7 Plus
+    /// - iPhone8: An iPhone 8
+    /// - iPhone8Plus: An iPhone 8 Plus
+    /// - iPhoneX: An iPhone X
+    /// - unsupported: An unsupported device (iPhone 6 or below)
+    internal enum Device: String {
+        case iPhone6S
+        case iPhone6SPlus
+        case iPhone7
+        case iPhone7Plus
+        case iPhone8
+        case iPhone8Plus
+        case iPhoneX
+        case unsupported
+    }
+    
+    // MARK: - Methods -
+    // MARK: Internal
+    
+    /// Checks which device (iPhone model) the user has
+    ///
+    /// - Returns: A `Device` enum value that represents the user's device
+    internal func getCurrentDevice() -> Device {
+        var sysinfo = utsname()
+        uname(&sysinfo)
+        guard let model = String(bytes: Data(bytes: &sysinfo.machine, count: Int(_SYS_NAMELEN)), encoding: .ascii)?.trimmingCharacters(in: .controlCharacters) else {
+            return .unsupported
+        }
+        switch model {
+        case "iPhone10,3", "iPhone10,6":
+            return .iPhoneX
+        case "iPhone10,2", "iPhone10,5":
+            return .iPhone8Plus
+        case "iPhone10,1", "iPhone10,4":
+            return .iPhone8
+        case "iPhone9,2", "iPhone9,4":
+            return .iPhone7Plus
+        case "iPhone9,1", "iPhone9,3":
+            return .iPhone7
+        case "iPhone8,2":
+            return .iPhone6SPlus
+        case "iPhone8,1":
+            return .iPhone6S
+        default:
+            return .unsupported
+        }
+    }
+    
+    
+    /// Checks whether the user's device has the Taptic Engine
+    ///
+    /// - Returns: `true` if the user's device has the Taptic Engine, `false` otherwise
+    internal func hasTapticEngine() -> Bool {
+        let supportedDevices = [Device.iPhone7, .iPhone7Plus, .iPhone8, .iPhone8Plus, .iPhoneX]
+        let currentDevice = getCurrentDevice()
+        return supportedDevices.contains(currentDevice)
+    }
+    
+    /// Checks whether the user's device has the Haptic Engine
+    ///
+    /// The devices that have a Taptic Engine can also create Haptic feedback, but we ignore those
+    /// devices because we want to use the Taptic Engine on those devices.
+    ///
+    /// - Returns: `true` if the user's device has the Haptic Engine, `false` otherwise
+    internal func hasHapticEngine() -> Bool {
+        let supportedDevices = [Device.iPhone6S, .iPhone6SPlus]
+        let currentDevice = getCurrentDevice()
+        return supportedDevices.contains(currentDevice)
+    }
 }
